4525e9777ae8dcb063e9632de9688e5003225afb
Seedling <david.r.graham32@gmail.com>
2025-01-04 12:05:01 -0500

    Set up clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto} from "next/font/google";
 import "./globals.css";
+import {ClerkProvider} from '@clerk/nextjs'
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -18,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${roboto.className}`}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
