5174d71ea798617ff450df5ce68992e5b4648643
Seedling <david.r.graham32@gmail.com>
2025-01-04 14:46:59 -0500

    Added database functionality to addTransaction Import {db} and use prisma built in functionality to call create() with the transaction data.

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -10,8 +10,7 @@ const AddTransaction = () => {
     if(error) {
       toast.error(error);
     } else {
-      alert('Transaction added');
-      console.log(data);
+      toast.success('Transaction added successfully!')
       
     }
     
