c30f73c4cf5a0c7fea766d55697a650742292774
Seedling <david.r.graham32@gmail.com>
2025-01-04 14:08:27 -0500

    Create add transaction Create component and separate action folder to hold the function.

---
+++ app/actions/addTransaction.ts
@@ -0,0 +1,34 @@
+'use server'
+
+interface TransactionData {
+  text: string;
+  amount: number;
+}
+
+interface TransactionResult {
+  data?: TransactionData;
+  error?: string;
+}
+
+async function addTransaction(formData: FormData): 
+Promise<TransactionResult> {
+  const textValue = formData.get('text');
+  const amountValue = formData.get('amount');
+
+  // check for input value
+  if(!textValue || textValue === '' || !amountValue) {
+    return {error: 'Text or amount is missing.'}
+  }
+
+  const text: string = textValue.toString(); // ensure text is string
+  const amount: number = parseFloat(amountValue.toString()); // parse amount as a number
+
+  const transactionData: TransactionData = {
+    text,
+    amount
+  };
+
+  return {data: transactionData};
+}
+
+export default addTransaction;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Guest from '@/components/Guest'
 import React from 'react'
 import { currentUser } from '@clerk/nextjs/server'
+import AddTransaction from '@/components/AddTransaction'
 
 const HomePage = async () => {
   const user = await currentUser();
@@ -11,6 +12,7 @@ const HomePage = async () => {
   return (
     <main>
       <h1>Welcome, {user.firstName}</h1>
+      <AddTransaction />
     </main>
   );
 }
