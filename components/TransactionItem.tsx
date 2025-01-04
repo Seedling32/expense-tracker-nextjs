aecff618487e5d1484b762e7a974196e40defc76
Seedling <david.r.graham32@gmail.com>
2025-01-04 17:27:58 -0500

    Create delete functionality Create deleteTransaction.tsx to use in the transactionItem when you click the 'x' button.

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,11 +1,29 @@
+'use client';
+
 import { Transaction } from "@/types/Transaction";
 import { addComas } from "@/lib/utils";
 import { toast } from "react-toastify";
-
-import React from 'react'
+import React from 'react';
+import deleteTransaction from "@/app/actions/deleteTransaction";
 
 const TransactionItem = ({ transaction}: {transaction: Transaction}) => {
   const sign = transaction.amount < 0 ? '-' : '+';
+
+  const handleDeleteTransaction = async (transactionId: string) => {
+    const confirmed = window.confirm('Are you sure you want to delete this transaction?');
+
+    if(!confirmed) {
+      return;
+    }
+
+    const { message, error} = await deleteTransaction(transactionId);
+
+    if(error) {
+      toast.error(error);
+    }
+
+    toast.success(message);
+  };
 
   return (
     <li className={ transaction.amount < 0 ? 'minus' : 'plus' }>
@@ -14,6 +32,7 @@ const TransactionItem = ({ transaction}: {transaction: Transaction}) => {
         {sign}
         { addComas(Math.abs(transaction.amount)) }
       </span>
+      <button onClick={() => handleDeleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
 }
